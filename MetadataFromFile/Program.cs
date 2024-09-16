2eb7eeb137a4f89d10cba66d027cf645a5f944a2
callebj <calle.bjureblad@student.gu.se>
2024-09-16 11:33:59 +0200

    Add project files.

---
+++ MetadataFromFile/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Vi vill skapa en console applikation som tar ett (sökväg) filnamn som input. 
+    Programmet ska sedan, genom att läsa in binärdata, 
+    avgöra om filen man angett är en .bmp fil eller en .png fil, 
+    samt ange höjd och bredd på bilden i pixlar. 
+    Om filen man angett varken är en .bmp eller .png så skriver vi det.
+    
+    Exempel output:
+    "File not found."
+    "This is not a valid .bmp or .png file!"
+    "This is a .png image. Resolution: 800x600 pixels."
+*/
+
+
+string inputPNG = @"C:\Users\calle\Pictures\pngBild.png";
+string inputBMP = @"C:\Users\calle\Pictures\bmpBild.bmp";
+
+
+byte[] byteInputPNG = File.ReadAllBytes(inputPNG);
+byte[] byteInputBMP = File.ReadAllBytes(inputBMP);
+
+foreach (byte b in byteInputPNG) Console.WriteLine(b);
---
+++ NET24/Program.cs
@@ -0,0 +1,16 @@
+﻿Cat[] catArray = new Cat[100];
+
+
+for (int i = 0; i < catArray.Length; i++)
+{
+    catArray[i] = new Cat() { name = $"Katt{i + 1}", age = i + 1 };
+    Console.WriteLine(catArray[i].name);
+}
+
+class Cat
+{
+    public string name = "default name";
+    public int age = 0;
+
+
+}
