23a6066d52bc959a7ba9454a6daa52c1029a2997
CV3B <calle@bjureblad.com>
2024-09-18 15:04:56 +0200

    Add ep uppgift

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,18 +1,67 @@
-﻿/*
- * Skriv en klass som representerar en person. 
- * Klassen ska ha två publika fält (fields): firstName, lastName. 
- * Skapa två instanser av personer med olika för och efternamn,
- * och använd sedan dessa för att printa ut (hela) namnen på de olika personerna.
- */
+﻿//https://projecteuler.net/problem=15
 
-Person p1 = new Person() { fristName = "Calle", lastName = "Bjureblad" };
-Person p2 = new Person() { fristName = "Kalle", lastName = "Njureblad" };
+int size = 10;
+int[,] grid = new int[size, size];
 
-Console.WriteLine($"{p1.fristName} {p1.lastName} | {p2.fristName} {p2.lastName}");
-class Person
+int stopRow = 1;
+int currRow = 0;
+int currCol = size;
+int tempCol = 0;
+
+for (int i = size; i > 0; i--)
 {
-    public string fristName;
-    public string lastName;
+    
+    for (int j = 0; j < i; j++)
+    {
+        //Console.Write(j + " ");
+        grid[currRow, j] = 1;
 
+        if (j == currCol-1)
+        {
+            for (int k = 0; k < size; k++) 
+            {
+                grid[k, currCol-1] = 1;
+                //if (currCol-2 == k)
+                //{
+                //    currCol++;
+                //}
+                //Console.WriteLine(stopRow + " J " + j + " K " + k);
+                //Console.WriteLine(k);
+                if (k == ) 
+                {
+                    //Console.WriteLine($"k: {k} | j: {j} | currCol {currCol}");
+                    stopRow++;
+                    tempCol = currCol;
+                    currCol++;
+                }
 
+            }
+            currCol = tempCol;
+            currCol--;
+        }
+
+    }
+    printRowNColumn(); 
+    Array.Clear(grid);
 }
+
+void printRowNColumn()
+{
+    //for (int h = 0; h < size; h++)
+    //{
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            Console.Write("Row {0} ", i); // Outside of the loop :)
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                Console.Write(string.Format("{0,6} ", grid[i, j]));
+            }
+            Console.Write("\n" + "\n");
+        }
+        Console.WriteLine();
+
+    //}
+}
+
+
+
